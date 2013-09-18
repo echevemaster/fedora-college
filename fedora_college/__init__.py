1f31735e78a5114bd547f0babf34041996cac4f2
Eduardo Echeverria <echevemaster@echevemaster.local>
2013-09-18 05:09:55 -0430

    Add parent directory to path

--- fedora_college/__init__.py
+++ fedora_college/__init__.py
@@ -3,6 +3,10 @@
 
 """:mod:`fedora_college` -- A virtual learning environment for Fedora
 """
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir) 
+import fedora_college
 
 from fedora_college import metadata
 
@@ -11,3 +15,4 @@ __version__ = metadata.version
 __author__ = metadata.authors[0]
 __license__ = metadata.license
 __copyright__ = metadata.copyright
+
