d655e293123af54a63d5858901d373ff27059029
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2013-11-08 00:46:30 -0430

    Unused import

--- fedora_college/core/forms.py
+++ fedora_college/core/forms.py
@@ -1,7 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, TextAreaField
 from wtforms.validators import Required
-from fedora_college.core.models import Screencast
 
 
 class AddScreenCast(Form):
