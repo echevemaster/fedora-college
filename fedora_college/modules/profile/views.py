e7cdcce9e6848807e9eda6c42ba98dd113fe687a
hammad <hammadhaleem@gmail.com>
2014-05-26 19:56:48 +0530

    Taken care of colliding blueprints

--- fedora_college/modules/profile/views.py
+++ fedora_college/modules/profile/views.py
@@ -1,86 +1,62 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import (Blueprint, render_template,
-                   request, redirect, url_for, g)
+                   request, redirect, url_for, g, jsonify)
 from fedora_college.core.database import db
-from fedora_college.modules.content.forms import *  # noqa
+from fedora_college.modules.profile.forms import *  # noqa
 from fedora_college.core.models import *  # noqa
+from flask.ext.babel import gettext
 from flask_fas_openid import fas_login_required
 
-bundle = Blueprint('content', __name__, template_folder='templates')
+
+bundle = Blueprint('profile', __name__, template_folder='templates',
+                   static_folder='static')
 
 
-@bundle.route('/content/add/', methods=['GET', 'POST'])
-@bundle.route('/content/add', methods=['GET', 'POST'])
-@bundle.route('/content/edit/<posturl>/', methods=['GET', 'POST'])
-@bundle.route('/content/edit/<posturl>', methods=['GET', 'POST'])
+@bundle.route('/user/edit', methods=['GET', 'POST'])
+@bundle.route('/user/<nickname>/edit', methods=['GET', 'POST'])
 @fas_login_required
-def addcontent(posturl=None):
+def editprofile(nickname=None):
+    if g.fas_user['username'] == nickname or request.method == 'POST':
+        user = UserProfile.query. \
+            filter_by(username=nickname).first()
 
-    form = CreateContent()
-    form_action = url_for('content.addcontent')
-    if posturl is not None:
-        try:
-            content = Content.query.filter_by(slug=posturl).first()
-            if content is None:
-                return str("Not-Found")
+        form = EditProfile(obj=user)
 
-            form = CreateContent(obj=content)
-            if form.slug.data == content and \
-               request.method == 'POST' and \
-               form.validate():
-                form.populate_obj(content)
-                db.session.commit()
-                return redirect(url_for('content.addcontent',
-                                        posturl=posturl, updated="True"))
-        except Exception as e:
-            return str("Not-Found : ") + str(e)
-
+        form_action = url_for('profile.editprofile')
+        if form.username.data == nickname and request.method == 'POST':
+            form.populate_obj(user)
+            print user.getdata()
+            db.session.commit()
+            return redirect(url_for('profile.user',
+                            nickname=nickname, updated="True"))
+        return render_template('profile/edit_user_profile.html', form=form,
+                               form_action=form_action, title="Update Profile")
     else:
-        if request.method == 'POST':
-            query = Content(form.title.data,
-                            form.slug.data,
-                            form.description.data,
-                            form.media_added_ids.data,
-                            form.active.data,
-                            form.tags.data,
-                            g.fas_user['username'],
-                            form.type_content.data
-                            )
-            try:
-                db.session.add(query)
-                db.session.commit()
-                # Duplicate entry
-            except Exception as e:
-                print e
-            print "Recieved", form.slug.data
-
-            return redirect(url_for('content.addcontent',
-                                    posturl=form.slug.data, updated="True"))
-        else:
-            print "Please validate form"
-    return render_template('content/edit_content.html', form=form,
-                           form_action=form_action, title="Create Content")
+        return "Unauthorised"
 
 
-@bundle.route('/blog', methods=['GET', 'POST'])
-@bundle.route('/blog/', methods=['GET', 'POST'])
-@bundle.route('/blog/<slug>/', methods=['GET', 'POST'])
-@bundle.route('/blog/<slug>', methods=['GET', 'POST'])
-def blog(slug=None):
-    if slug is not None:
-        try:
-            posts = Content.query. \
-                filter_by(type_content="blog").all()
-        except:
-            posts = "No such posts in database."
-    else:
+@bundle.route('/user/<nickname>')
+@fas_login_required
+def user(nickname):
 
-        try:
-            posts = Content.query. \
-                filter_by(type_content="blog").all()
-        except:
-            posts = "Databse is empty"
+    msg = ""
+    if request.args.get('updated') == "True":
+        msg = msg + "Profile Updated"
+        print msg
+    user = UserProfile.query. \
+        filter_by(username=nickname).first()
+    if user is None:
+        return jsonify({gettext('User'): str(nickname)+gettext('not found.')})
 
-    return render_template('blog/index.html',
-                           title='Blog',
-                           content=posts)
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('profile/user_profile.html',
+                           user=user,
+                           posts=posts,
+                           url=str(
+                               url_for(
+                                   'profile.editprofile', nickname=nickname,)),
+                           message=msg)
