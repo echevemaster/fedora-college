818ac3e76977a011c76b5bcd359e0f26981e5c7e
hammad <hammadhaleem@gmail.com>
2014-06-15 12:33:15 +0530

    Resolved error with create content

--- fedora_college/modules/content/views.py
+++ fedora_college/modules/content/views.py
@@ -9,13 +9,23 @@ from fedora_college.modules.content.forms import *  # noqa
 from fedora_college.core.models import *  # noqa
 from flask_fas_openid import fas_login_required
 
-
-_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
-
 bundle = Blueprint('content', __name__, template_folder='templates')
 
 
 from fedora_college.modules.content.media import *  # noqa
+
+
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+
+def slugify(text, delim=u'-'):
+    """Generates an slightly worse ASCII-only slug."""
+    result = []
+    for word in _punct_re.split(text.lower()):
+        word = normalize('NFKD', word).encode('ascii', 'ignore')
+        if word:
+            result.append(word)
+    return unicode(delim.join(result))
 
 
 def attach_tags(tags, content):
@@ -29,37 +39,23 @@ def attach_tags(tags, content):
     db.session.commit()
 
 
-def slugify(text, delim=u'-'):
-    """Generates an slightly worse ASCII-only slug."""
-    result = []
-    for word in _punct_re.split(text.lower()):
-        word = normalize('NFKD', word).encode('ascii', 'ignore')
-        if word:
-            result.append(word)
-    return unicode(delim.join(result))
-
-
 @bundle.route('/content/add/', methods=['GET', 'POST'])
 @bundle.route('/content/add', methods=['GET', 'POST'])
 @bundle.route('/content/edit/<posturl>/', methods=['GET', 'POST'])
 @bundle.route('/content/edit/<posturl>', methods=['GET', 'POST'])
 @fas_login_required
 def addcontent(posturl=None):
-    media = Media.query.filter_by(user_id=g.fas_user['username']).all()
-
     form = CreateContent()
     form_action = url_for('content.addcontent')
-
     if posturl is not None:
         content = Content.query.filter_by(slug=posturl).first_or_404()
         form = CreateContent(obj=content)
-        if content.slug is posturl and form.validate_on_submit():
+        if form.validate_on_submit():
             form.populate_obj(content)
             tags = str(form.tags.data).split(',')
 
             attach_tags(tags, content)
             db.session.commit()
-
             return redirect(url_for('content.addcontent',
                                     posturl=posturl,
                                     updated="Successfully updated")
@@ -67,16 +63,8 @@ def addcontent(posturl=None):
 
     else:
         if form.validate_on_submit():
-            slug = slugify(form.title.data)
-            try:
-                content = Content.query.filter_by(slug=posturl).first_or_404()
-                if content.data:
-                    date = str(time.strftime("%d/%m/%Y %S"))
-                    slug = slugify(form.title.data + date)
-            except:
-                pass
             query = Content(form.title.data,
-                            slug,
+                            slugify(form.title.data),
                             form.description.data,
                             form.media_added_ids.data,
                             form.active.data,
@@ -93,21 +81,16 @@ def addcontent(posturl=None):
                 # Duplicate entry
             except Exception as e:
                 print e
-                db.session.rollback()
-                db.session.flush()
+                return str(e)
 
             return redirect(url_for('content.addcontent',
-                                    posturl=slug,
-                                    media=media,
+                                    posturl=slugify(form.title.data),
                                     updated="Successfully updated")
                             )
         else:
             print "Please validate form"
-    return render_template('content/edit_content.html',
-                           form=form,
-                           form_action=form_action,
-                           media=media,
-                           title="Create Content")
+    return render_template('content/edit_content.html', form=form,
+                           form_action=form_action, title="Create Content")
 
 
 @bundle.route('/blog', methods=['GET', 'POST'])
