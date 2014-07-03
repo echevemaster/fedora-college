0e1c60f4f21e8287f18a225ed7841f3ce19e78ef
hammad <hammadhaleem@gmail.com>
2014-07-03 10:01:01 +0530

    Updated metadata

--- fedora_college/__init__.py
+++ fedora_college/__init__.py
@@ -5,16 +5,16 @@ sys.path[0:0] = [""]
 from flask import Flask
 from flask.ext.babel import Babel
 from flask import request
-#from fedora_college import metadata
+from fedora_college.metadata import * #noqa
 from fedora_college.core.constructor import (build_app as build_fedora,
                                              authenticated,
                                              logger, is_admin)
 
 
-#__version__ = metadata.version
-#__author__ = metadata.authors[0]
-#__license__ = metadata.license
-#__copyright__ = metadata.copyright
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
 
 
 LANGUAGES = {
@@ -32,7 +32,6 @@ def build_app(app):
 
 
 apps = Flask(__name__)
-#manager = Manager(app)
 babel = Babel(apps)
 
 
@@ -41,35 +40,14 @@ def get_locale():
     return request.accept_languages.best_match(LANGUAGES.keys())
 
 
-app = build_app(apps)
- 
-@app.context_processor
+@apps.context_processor
 def inject_variables():
     user_admin = is_admin(app)
 
     return dict(
         is_admin=user_admin)
 
-'''
-@manager.command
-def create_database():
-    """Create all database tables"""
-    create_db(app)
 
-
-@manager.command
-def drop_database():
-    """Create all database tables"""
-    drop_db(app)
-
-
-@manager.command
-def run():
-    """Run application"""
-    app.run(debug=True, host='0.0.0.0')
-'''
+app = build_app(apps)
 if __name__ == '__main__':
-    #build_app(app)
-    #is_admin(app)
     app.run()
-    #app.run(debug=True, host='0.0.0.0')
--- fedora_college/metadata.py
+++ fedora_college/metadata.py
@@ -12,9 +12,10 @@ project = "Fedora College"
 project_no_spaces = project.replace(' ', '')
 version = '0.1'
 description = 'A virtual learning environment for Fedora'
-authors = ['Eduardo Echeverria', 'Yohan Graterol']
+authors = ['Eduardo Echeverria', 'Yohan Graterol', 'Hammad Haleem']
 authors_string = ', '.join(authors)
-emails = ['echevemaster@fedoraproject.org', 'yograterol@fedoraproject.org']
+emails = ['echevemaster@fedoraproject.org',
+          'yograterol@fedoraproject.org', 'hammadhaleem@fedoraproject.org']
 license = 'BSD'
 copyright = '2013 ' + authors_string
 url = 'https://github.com/echevemaster/fedora-college'
