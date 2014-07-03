15a9c0db22dd50b702409fdd6e0ee0abc18aa4ba
hammad <hammadhaleem@gmail.com>
2014-07-03 20:49:15 +0530

    updated __init__

--- fedora_college/__init__.py
+++ fedora_college/__init__.py
@@ -5,16 +5,32 @@ sys.path[0:0] = [""]
 from flask import Flask
 from flask.ext.babel import Babel
 from flask import request
-from fedora_college import metadata
 from fedora_college.core.constructor import (build_app as build_fedora,
                                              authenticated,
                                              logger, is_admin)
 
 
-__version__ = metadata.version
-__author__ = metadata.authors[0]
-__license__ = metadata.license
-__copyright__ = metadata.copyright
+package = 'fedora_college'
+project = "Fedora College"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'A virtual learning environment for Fedora'
+authors = ['Eduardo Echeverria',
+           'Yohan Graterol',
+           'Hammad Haleem']
+authors_string = ', '.join(authors)
+emails = ['echevemaster@fedoraproject.org',
+          'yograterol@fedoraproject.org',
+          'hammadhaleem@fedoraproject.org']
+license = 'BSD'
+copyright = '2013 ' + authors_string
+url = 'https://github.com/echevemaster/fedora-college'
+
+
+__version__ = version
+__author__ = authors[0]
+__license__ = license
+__copyright__ = copyright
 
 
 LANGUAGES = {
--- fedora_college/metadata.py
+++ fedora_college/metadata.py
@@ -12,10 +12,13 @@ project = "Fedora College"
 project_no_spaces = project.replace(' ', '')
 version = '0.1'
 description = 'A virtual learning environment for Fedora'
-authors = ['Eduardo Echeverria', 'Yohan Graterol', 'Hammad Haleem']
+authors = ['Eduardo Echeverria',
+           'Yohan Graterol',
+           'Hammad Haleem']
 authors_string = ', '.join(authors)
 emails = ['echevemaster@fedoraproject.org',
-          'yograterol@fedoraproject.org', 'hammadhaleem@fedoraproject.org']
+          'yograterol@fedoraproject.org',
+          'hammadhaleem@fedoraproject.org']
 license = 'BSD'
 copyright = '2013 ' + authors_string
 url = 'https://github.com/echevemaster/fedora-college'
